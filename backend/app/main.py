91e4bd59208f5f834c181739749945d2619b33da
Tejas Nikhar <tejastn10@gmail.com>
2025-07-27 08:35:00 +0530

    feat: update main file

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,31 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, status
+from fastapi.encoders import jsonable_encoder
+from fastapi.exceptions import RequestValidationError
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+from starlette.exceptions import HTTPException as StarletteHTTPException
+
+from app.config.core import settings
 
 from app.routers import health
 
 app = FastAPI(
-    title="Papyrus",
-    description="Secure API for removing and adding passwords from and to PDF",
-    version="0.0.1",
+    title=settings.PROJECT_NAME,
+    description=settings.DESCRIPTION,
+    version=settings.VERSION,
+    openapi_url=f"{settings.API_V1_STR}/openapi.json",
+    docs_url="/docs",
+    redoc_url="/redoc",
 )
+
+# Set all CORS enabled origins
+if settings.BACKEND_CORS_ORIGINS:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
 app.include_router(health.router)
