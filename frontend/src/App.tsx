7a3f06cecc03b902795898da304e2862c515857e
Tejas Nikhar <tejastn10@gmail.com>
2025-08-14 08:30:00 +0530

    feat: update App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,11 @@
 import { FC } from "react";
 
+import styled from "styled-components";
 import { Image, Layout } from "antd";
 
 import { ThemeProvider } from "./theme/ThemeProvider";
+
+import { Main } from "./containers/Main";
 
 import LogoSvg from "./asset/logo.svg";
 
@@ -12,52 +15,62 @@ const App: FC = () => {
 	return (
 		<>
 			<ThemeProvider>
-				<Layout style={{ minHeight: "100vh", width: "100%" }}>
-					<Header
-						style={{
-							height: "10vh",
-							padding: "2rem 4rem",
-							background: "transparent",
-							boxShadow: "0 4px 8px -2px rgba(255, 255, 255, 0.15)",
-
-							display: "flex",
-							alignItems: "center",
-							justifyContent: "center",
-
-							fontSize: "2.5rem",
-							fontWeight: "bolder",
-						}}
-					>
-						<Image
-							src={LogoSvg}
-							alt="Logo"
-							preview={false}
-							width={48}
-							style={{ paddingRight: "1rem" }}
-						/>
+				<LayoutContainer>
+					<HeaderContainer>
+						<ImageContainer src={LogoSvg} alt="Logo" preview={false} width={48} />
 						Papyrus
-					</Header>
-					<Content style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
-						Main Body
-					</Content>
-					<Footer
-						style={{
-							height: "10vh",
-							boxShadow: "0 -4px 8px -2px rgba(255, 255, 255, 0.15)",
-
-							display: "flex",
-							alignItems: "center",
-							justifyContent: "center",
-
-							fontWeight: "lighter",
-						}}
-					>
-						⬜️ Papyrus - Secure, Fast and Privacy-First ⬜️
-					</Footer>
-				</Layout>
+					</HeaderContainer>
+					<ContentContainer>
+						<Main />
+					</ContentContainer>
+					<FooterContainer>⬜️ Papyrus - Secure, Fast and Privacy-First ⬜️</FooterContainer>
+				</LayoutContainer>
 			</ThemeProvider>
 		</>
 	);
 };
 
 export { App };
+
+const LayoutContainer = styled(Layout)`
+	width: 100%;
+	min-height: 100vh;
+`;
+
+const HeaderContainer = styled(Header)`
+	height: 10vh;
+	padding: 2rem 4rem;
+	background: transparent;
+
+	display: flex;
+	align-items: center;
+	justify-content: center;
+
+	font-size: 2.5rem;
+	font-weight: bolder;
+	box-shadow: 0 4px 8px -2px rgba(255, 255, 255, 0.15);
+`;
+
+const ImageContainer = styled(Image)`
+	padding-right: 1rem;
+`;
+
+const ContentContainer = styled(Content)`
+	width: 100%;
+	height: 100%;
+	display: flex;
+	flex-direction: column;
+	align-items: stretch;
+	justify-content: center;
+`;
+
+const FooterContainer = styled(Footer)`
+	height: 10vh;
+
+	display: flex;
+	align-items: center;
+	justify-content: center;
+
+	font-weight: lighter;
+	box-shadow: 0 -4px 8px -2px rgba(255, 255, 255, 0.15);
+`;
